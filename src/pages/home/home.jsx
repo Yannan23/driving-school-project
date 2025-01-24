393842739ab5c9642ef58c64bd56f321e8db8d26
Yannan23 <yannansun0723@gmail.com>
2025-01-24 17:12:03 +1100

    created components and moved header and footer to components file

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,5 @@
-import Header from './header/header'
-import Footer from './footer/footer'
+import Header from '../../components/header/header'
+import Footer from '../../components/footer/footer'
 import Main from './main/main'
 
 function Home() {
