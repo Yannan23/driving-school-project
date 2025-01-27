98f88f549248bd955efcfdc1e019904f854d36e9
Yannan23 <yannansun0723@gmail.com>
2025-01-28 04:46:11 +1100

    initialized footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
 import './footer.css';
+import logo from '/src/assets/logo.svg';
 
 const Footer = () => {
     return (
-        <footer className='container' >
-            <div className=''>
-                <div className=''>
-                    <div className='grid grid-cols-2 bg-black h-6'>
-                        <a href="">
-                            <img src="/src/assets/logo.svg" alt="" />
-                        </a>
+        <footer className='bg-dark-color text-white-color'>
+            <div className='m-16'>
+                <div className='flex'>
+                    <div className='shrink-0'>
+                        <img src="/src/assets/logo.svg" alt="logo" />
                     </div>
-                    <div className='grid grid-cols-6 bg-slate-700'>
-
+                    <div>
+                        <h2>Subscribe to our newsletter</h2>
+                        <form action="" className='flex flex-col'>
+                            <label htmlFor="footer-email">Add your email below and we will make sure you have
+                                the latest news</label>
+                            <input type="email" id="footer-email" placeholder="Enter your email" />
+                            <div className="footer-btn">
+                                <button>Subscribe</button>
+                            </div>
+                        </form>
                     </div>
-                    <div className='grid grid-cols-4 bg-slate-500'>
-
+                </div>
+                <div>
+                    <h2>Contact Info</h2>
+                    <div className='flex'>
+                        <i className="fa-regular fa-envelope"></i>
+                        <p>khanrashed2411@gmail.com</p>
                     </div>
-
+                    <div className='flex'>
+                        <i className="fa-solid fa-phone"></i>
+                        <p>0470272595</p>
+                    </div>
                 </div>
             </div>
-
+            <div className="w-full border-t border-white border-solid my-4">
+                <div className='flex'>
+                    <div className='flex'>
+                        <p>© 2025 NSW Safe Driving Schoo</p>
+                        <p>KeyWork</p>
+                    </div>
+                    <div>
+                        <p>Terms & Condition</p>
+                        <p>Copy Right and Logo Trade mark reserved .</p>
+                    </div>
+                </div>
+            </div>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
