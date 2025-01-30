5365e53cf2c6fe91a3b30bb05ed2fdf27603ea17
Yannan23 <yannansun0723@gmail.com>
2025-01-30 18:39:18 +1100

    home page our-story session done

--- src/pages/home/main/our-story/our-story.jsx
+++ src/pages/home/main/our-story/our-story.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './our-story.css';
 
 const OurStory = () => {
     return (
-        <div>
-            <h1>OurStory</h1>
-        </div>
+        <session className="our-story-container">
+            <img src="/src/assets/nsw safe 1.jpg" alt="" />
+            <div className='our-story-content'>
+                <h2 className='poppins-semibold capitalize'>OUR STORY</h2>
+                <p className='poppins-regular'>Welcome to <span className='roboto-condensed'>NSW SAFE Driving School</span>, we teach to drive is not just about getting a driving licence but is an essential skill for rest of the life. Driving is our DNA and we are  so successful because we deliver extraordinary individually structured lessons very carefully & effortlessly which is great value for students. Our instructor are extremely extraordinary professional and attentive while they teach lessons .</p>
+            </div>
+        </session>
     )
 }
 
