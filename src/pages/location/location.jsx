47d582aaa0593901b23893f9c059ea13ef1fbecb
Yuxian Du <yuxiandu5@gmail.com>
2025-02-12 13:42:36 +1100

    map added

--- src/pages/location/location.jsx
+++ src/pages/location/location.jsx
@@ -1,11 +1,13 @@
 import LocationBanner from './location-components/location-banner/location-banner';
 import LocationServiceArea from './location-components/location-service-area/location-service-area';
 import LocationFooter from './location-components/location-footer/location-footer';
+import LocationMap from './location-components/location-map/location-map';
 
 const Location = () => {
     return (
         <div>
             <LocationBanner/>
+            <LocationMap/>
             <LocationServiceArea/>
             <LocationFooter/>
         </div>
