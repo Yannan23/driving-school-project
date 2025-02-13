175a9e360d31950e4698f176cac96bd163f0acbc
Yannan23 <yannansun0723@gmail.com>
2025-02-13 16:29:15 +1100

    Updated the navbar contact info

--- src/components/buttons/call-button.jsx
+++ src/components/buttons/call-button.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const CallButton = () => {
     const handleClick = () => {
         window.location.href = 'tel:+61470272595'
@@ -7,7 +5,7 @@ const CallButton = () => {
 
     return (
         <button className="!bg-transparent hover:bg-transparent">
-            <p onClick={handleClick} className='hover:text-amber-400 cursor-pointer font-Roboto-Condensed'>0470272595</p>
+            <p onClick={handleClick} className='hover:text-amber-400 hover:underline cursor-pointer font-Roboto-Condensed'>0470272595</p>
         </button>
     )
 }
